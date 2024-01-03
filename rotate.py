cd3a6eef76d7c6bd727d63d03b42a02c0255f141
pranjal singh <singhpranjal2312@gmail.com>
2024-01-04 00:52:35 +0530

    Rotation function

--- rotate.py
+++ rotate.py
@@ -1,11 +1,2 @@
-
-arr = [1,2,3,4,5]
-k = int(input("enter the k"))
-n = len(arr)
-
-for i in range(n):
-    t= arr[i]
-    arr[i]=arr[(n-k+i)%n]
-    print(arr)
-        
-
+s = input("word")
+print(s.title())
