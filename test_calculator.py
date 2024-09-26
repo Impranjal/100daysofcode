4963413919edc345d3fe8e4e9d91c4cbce9029db
pranjal singh <singhpranjal2312@gmail.com>
2024-09-26 23:55:11 +0530

    Test case addition

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,13 @@
 """Practice the pytest .."""
 import pytest
 import calculator as m
-def test_add():
-    result = m.py(2,4)
-    assert result == 6
+class Testclac:
+    def test_multi(self):
+        result = m.multi(2,4)
+        result == 8
+    def test_sub(self):
+        result = m.sub(4,2)
+        result == 2
+    def test_add(self):
+        result = m.add(2,4)
+        assert result == 6
