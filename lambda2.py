2655360c07411fae9a419d210edea2b9d7eb864e
pranjal singh <singhpranjal2312@gmail.com>
2024-09-28 22:25:03 +0530

    lambda function

---
+++ lambda2.py
@@ -0,0 +1,4 @@
+array = [1,2,3,4]
+
+new_array = map((lambda array: array**2),array)
+print(list(new_array))
---
+++ oops1.py
@@ -0,0 +1,7 @@
+class Employee:
+    pass
+emp= Employee()
+print(emp)
+
+emp.first="corey"
+emp.last ="schafer"
