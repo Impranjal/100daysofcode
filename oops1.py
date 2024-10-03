63bad61b71046f8fde585c2eae0bed7948adc7c9
pranjal singh <singhpranjal2312@gmail.com>
2024-10-03 11:51:01 +0530

    oops 1

--- oops1.py
+++ oops1.py
@@ -1,7 +1,18 @@
-class Employee:
-    pass
-emp= Employee()
-print(emp)
+class employee:
+    def __init__(self,first,last,pay):
+        self.first = first
+        self.last = last
+        self.pay =pay
+        self.email = first + '.' +last +'@company.com'
 
-emp.first="corey"
-emp.last ="schafer"
+    def full_name(self):
+        return "{} {}".format(self.first,self.last)
+    def apply_raise(self):
+        self.pay = int(self.pay*1.04)
+            
+    @classmethod
+    def set_raise_amt(cls,amount):
+        pass   
+
+emp1= employee("pranjal","singh",4000)
+print(emp1.full_name())
