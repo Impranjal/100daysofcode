5c69639896bb846c67a363dd490bc99949362f7d
pranjal singh <singhpranjal2312@gmail.com>
2024-10-03 22:24:53 +0530

    oops 1

--- oops1.py
+++ oops1.py
@@ -20,9 +20,37 @@ class employee:
             return False
         return True
 
-emp1= employee("pranjal","singh",4000)
-print(emp1.full_name())
-import datetime
-my_date = datetime.date(2016,7,10)
+# emp1= employee("pranjal","singh",4000)
+# print(emp1.full_name())
+# import datetime
+# my_date = datetime.date(2016,7,10)
 
-print(employee.is_workday(my_date))
+# print(employee.is_workday(my_date))
+
+class Deveolper(employee):
+    raise_amt = 1.10
+    def __init__(self,first,last,pay,prog_lang):
+        super().__init__(first,last,pay)
+        self.prog_lang =prog_lang
+    
+class Manager(employee):
+    def __init__(self,first,last,pay,employee_list):
+        super().__init__(first,last,pay)
+        if employee_list is None:
+            self.employee_list = []
+        else:
+            self.employee_list = employee_list
+    def add_emp(self,emp):
+        if emp not in self.employee_list:
+            self.employee_list.append(emp)
+    def remove_emp(self,emp):
+        if emp  in self.employee_list:
+            self.employee_list.remove(emp)
+    def print_emp(self):
+        for emp in self.employee_list:
+            print(f'----> {emp}')
+        
+
+mang = Manager('akash','hemanth',9000,['raghul'])
+mang.add_emp('saad')
+print(mang.print_emp())
