a8dec9d751560178386c586911db5e5c03d2a373
pranjal singh <singhpranjal2312@gmail.com>
2025-03-20 22:27:07 +0530

    First occurenece

---
+++ first_ocurrence.py
@@ -0,0 +1,21 @@
+def first(arr,x):
+    low =0
+    high = len(arr)-1
+    while low <= high:
+        mid = low+(high-low)//2
+        
+        if arr[mid] > x:
+            high = mid -1
+        elif arr[mid]<x:
+            low = mid +1
+        else:
+            if mid==0 or arr[mid-1] !=arr[mid]:
+                return mid
+            else:
+                high = mid-1
+    return -1
+    
+
+arr= [1,2,2,2,2,2,3,3]
+x=2
+print(first(arr,x))
